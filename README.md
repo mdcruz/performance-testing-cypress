397a4db189207c04ceab848fad71013e318d957d
mdcruz <marie.descruz@gmail.com>
2021-01-08 00:23:16 +0000

    code refactoring and added useful links

--- README.md
+++ README.md
@@ -16,3 +16,10 @@ To get started on the hands-on material, simply clone this repository, `cd` to t
 To open the Cypress Test Runner, simply type `npx cypress open` and select the file that you want to run.
 
 Alternatively, to run it headlessly on Chrome, type `npx cypress run --browser chrome --headless`
+
+## Links from the recorded session
+
+1. [cypress-audit](https://github.com/mfrachet/cypress-audit)
+2. [lighthouse-cli](https://github.com/GoogleChrome/lighthouse)
+3. [Cypress Browser Launch API](https://docs.cypress.io/api/plugins/browser-launch-api.html#Syntax)
+4. [Lighthouse Emulation](https://github.com/GoogleChrome/lighthouse/blob/master/docs/emulation.md)
--- cypress/integration/cypress-audit.spec.js
+++ cypress/integration/cypress-audit.spec.js
@@ -1,28 +1,29 @@
 /// <reference types="Cypress" />
 
-describe('Photobiz website', () => {
-  it('should run lighthouse performance audits using default thresholds', () => {
+describe('Photobiz Lighthouse', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
+
+  it('should run performance audits', () => {
     cy.lighthouse();
   });
 
-  it('should run lighthouse performance audits using custom thresholds', () => {
-    const thresholds = {
+  it('should run performance audits using custom thresholds', () => {
+    const customThresholds = {
       performance: 50,
-      accessibility: 80,
+      accessibility: 50,
+      seo: 70,
       'first-contentful-paint': 2000,
       'largest-contentful-paint': 3000,
-      interactive: 2000,
-      seo: 60,
-      pwa: 50,
+      'cumulative-layout-shift': 0.1,
+      'total-blocking-time': 500,
     };
 
-    const lighthouseConfig = {
+    const desktopConfig = {
       formFactor: 'desktop',
       screenEmulation: { disabled: true },
     };
-
-    cy.visit('/');
-    cy.lighthouse(thresholds, lighthouseConfig);
+    cy.lighthouse(customThresholds, desktopConfig);
   });
 });
